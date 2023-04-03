0895a2912f0c45e9e78c6c29b5ac8990a3270e15
jordan.p <jordan.p@getlinked.io>
2023-04-03 10:46:39 +0300

    added error descriptions

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -2,14 +2,14 @@ function authMiddleware(req, res, next) {
   const authHeader = req.headers.authorization;
   
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ error: 'Unauthorized' });
+    return res.status(401).json({ error: 'Your authorization is not Bearer.' });
   }
 
   const token = authHeader.substring(7);
   const tokenExpirationTime = req.app.locals.tokens[token];
 
   if (!tokenExpirationTime || Date.now() > tokenExpirationTime) {
-    return res.status(401).json({ error: 'Unauthorized' });
+    return res.status(401).json({ error: 'Token expiration time has passed, sign in again to get new token.' });
   }
 
   req.locals = { token };
